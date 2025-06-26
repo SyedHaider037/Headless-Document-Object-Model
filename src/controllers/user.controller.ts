f3e099c0313f908f1cc4153d2e02e77d3f38d9f5
SyedHaider037 <syedhaider037@gmail.com>
2025-06-26 23:03:41 +0500

    Add a refresh Token  route for user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,11 +8,15 @@ import { or, eq } from "drizzle-orm";
 import { hashPassword, verifyPassword, generateAccessToken, generateRefreshToken } from "../lib/jwt&bcryptAuth";
 import { registerSchema, loginSchema } from "../validators/user.validSchema.ts";
 import { UserRoles } from "../constants/rolesEnum.ts";
+import jwt from "jsonwebtoken";
 
 interface RequestWithUser extends Request {
     user: typeof users.$inferSelect;
 }
 
+type MyToken = {
+  id: string;
+}
 export const registerUser = asyncHandler( async (req: Request, res: Response) => {
     const parsed = registerSchema.safeParse(req.body);
 
@@ -137,3 +141,59 @@ export const logoutUser = asyncHandler( async (req: Request, res: Response) => {
         .json(new ApiResponse(200,{},"User Logged Out Successfully."))
 });
 
+
+
+export const refreshAccessToken = asyncHandler ( async (req: Request, res: Response) => {
+    const incomingRequest = req.cookies?.refreshToken || req.body?.refreshToken;
+
+    if (!incomingRequest) {
+        throw new ApiError(401,"Unauthorized Request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRequest, process.env.REFRESH_TOKEN_SECRET!) as MyToken;
+    
+        const [existedUser] = await db.select().from(users).where(eq(users.id, decodedToken.id))
+    
+        if (!existedUser) {
+            throw new ApiError(401,"User doesnot exits.")
+        }
+    
+        if (incomingRequest !== existedUser?.refreshToken) {
+            throw new ApiError(400, "Refresh Token is expired")
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+        
+        const accessToken = generateAccessToken({
+            id: existedUser.id,
+            username: existedUser.username,
+            email: existedUser.email,
+            role: existedUser.role,
+        });
+
+        const newRefreshToken = generateRefreshToken({id: existedUser.id});
+
+        await db.update(users)
+            .set({refreshToken: newRefreshToken})
+            .where(eq(users.id , existedUser.id));
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken: newRefreshToken},
+                "Access token refreshed"
+            )
+        )
+    } catch (error: any) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+
+})
